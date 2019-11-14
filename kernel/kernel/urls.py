920e3d784db3abab8923e3b63ad9ababc07b0dc9
ihaveint <ihaveint@gmail.com>
2019-11-15 00:49:06 +0330

    Add api urls to krenel urls

--- kernel/kernel/urls.py
+++ kernel/kernel/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from quiz.api.urls import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^api-auth/', include('rest_framework.urls')),
+    re_path(r'^api/v1/', include(router.urls)),
 ]
